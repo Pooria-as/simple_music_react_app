1c611a2791c04fddc2ff34993e9d4b3d60ed0ae1
Pooria <p.asiabi@gmail.com>
2021-10-17 21:48:25 +0330

    😁

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {NavLink} from "react-router-dom"
+const Home =()=>
+(
+    <header>
+        <NavLink to='/'>
+            Music
+        </NavLink>
+    </header>
+);
+
+
+export default Home
